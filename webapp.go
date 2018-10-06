b22de01586f53af1d35a253c1c6925068d458d9f
Jason Y. Lee <jylee@cs.ucr.edu>
2018-10-05 20:54:53 -0700

    Wrap current time in a string

--- webapp.go
+++ webapp.go
@@ -9,7 +9,7 @@ import (
 
 func webTime(w http.ResponseWriter, r *http.Request) {
   t := time.Now().UTC()
-  message := fmt.Sprintf("{ \"current_time\": %s }", t.Format(time.RFC1123))
+  message := fmt.Sprintf("{ \"current_time\": \"%s\" }", t.Format(time.RFC1123))
   w.Write([]byte(message))
 }
 
