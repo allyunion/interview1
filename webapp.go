49decc2a22b1c8608ec22d11105d0dab826e164d
Jason Y. Lee <jylee@cs.ucr.edu>
2018-10-07 13:38:59 -0700

    Formatting

--- webapp.go
+++ webapp.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "net/http"
-  "time"
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 func webTime(w http.ResponseWriter, r *http.Request) {
-  t := time.Now().UTC()
-  message := fmt.Sprintf("{ \"current_time\": \"%s\" }", t.Format(time.RFC1123))
-  w.Write([]byte(message))
+	t := time.Now().UTC()
+	message := fmt.Sprintf("{ \"current_time\": \"%s\" }", t.Format(time.RFC1123))
+	w.Write([]byte(message))
 }
 
 func main() {
-  portPtr := flag.Int("port", 8080, "port to use")
-  flag.Parse()
-  port := fmt.Sprintf(":%v", *portPtr)
-  fmt.Printf("Starting service on port %v...\n", *portPtr)
-  http.HandleFunc("/", webTime)
-  if err := http.ListenAndServe(port, nil); err != nil {
-    panic(err)
-  }
+	portPtr := flag.Int("port", 8080, "port to use")
+	flag.Parse()
+	port := fmt.Sprintf(":%v", *portPtr)
+	fmt.Printf("Starting service on port %v...\n", *portPtr)
+	http.HandleFunc("/", webTime)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		panic(err)
+	}
 }
